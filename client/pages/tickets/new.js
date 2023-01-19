0b396886d61e33fe32ca97db7d6910b57d40e47f
ARPITxxPATEL <arpitkumar2105@gmail.com>
2023-01-19 23:46:35 +0530

    client side ticket creation

---
+++ client/pages/tickets/new.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import useRequest from '../../hooks/use-request';
+
+const NewTicket = () => {
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/tickets',
+    method: 'post',
+    body: {
+      title,
+      price,
+    },
+    onSuccess: (ticket) => console.log(ticket),
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    doRequest();
+  };
+
+  const onBlur = () => {
+    const value = parseFloat(price);
+
+    if (isNaN()) {
+      return;
+    }
+    setPrice(value.toFixed(2));
+  };
+
+  return (
+    <div>
+      <h1>Create a new ticket</h1>
+      <form onSubmit={onSubmit}>
+        <div classname="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            classname="form-control"
+          />
+        </div>
+        <div classname="form-group">
+          <label>Title</label>
+          <input
+            value={price}
+            onBlur={onBlur}
+            onChange={(e) => setPrice(e.target.value)}
+            classname="form-control"
+          />
+        </div>
+        {errors}
+        <button classname="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default NewTicket;
